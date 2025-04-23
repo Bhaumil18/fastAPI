f7d2d7e3b236ae3af0bb26deffe1b16c08855b57
Bhaumil Patel <139143315+Bhaumil18@users.noreply.github.com>
2025-04-23 12:32:34 +0530

    Update get_client.py

--- app/configs/get_client.py
+++ app/configs/get_client.py
@@ -7,5 +7,6 @@ from clickhouse_connect import get_client
 
 
 def get_clickhouse_client():
-    return get_client(host='192.168.0.10',port=8123)
+    # return get_client(host='192.168.0.10',port=8123)
+    return get_client(host='c967-116-72-8-158.ngrok-free.app', port=80)
 
